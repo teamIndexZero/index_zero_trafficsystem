5feb54ce23860a43044d99e6236840cf3b4f801b
E A. Davison <axedude1@gmail.com>
2016-02-02 09:19:09 +0000

    Added FileIO class to Logger and updated UML class diag. to reflect that

---
+++ Logger/src/Logger/FileIO/FileIO.java
@@ -0,0 +1,27 @@
+package Logger.FileIO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Created by Es on 02/02/2016.
+ */
+public class FileIO { //TODO
+    /**
+     * Constructor
+     * @param file_name File name
+     * @throws IOException when File cannot be opened
+     */
+    public FileIO( String file_name ) throws IOException {
+        try {
+            List<String> lines = Files.readAllLines((Paths.get(file_name)));
+        } catch ( IOException e ) {
+            //TODO LOg that crap
+            //System.out.println(e.getMessage()); e.printStackTrace(); throw e;
+            //TODO throw back
+        }
+
+    }
+}
