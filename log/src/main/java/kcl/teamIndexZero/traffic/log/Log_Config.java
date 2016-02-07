bcba8ecf2e9394995d11946df3f3c23230f24a0d
E A. Davison <axedude1@gmail.com>
2016-02-07 17:21:05 +0000

    Some clean up of the Log_Config and beginning of implementation for the log configuration file loader/checker

--- log/src/main/java/kcl/teamIndexZero/traffic/log/Log_Config.java
+++ log/src/main/java/kcl/teamIndexZero/traffic/log/Log_Config.java
@@ -1,11 +1,14 @@
 package kcl.teamIndexZero.traffic.log;
 
+import kcl.teamIndexZero.traffic.log.fileIO.FileInput;
 import kcl.teamIndexZero.traffic.log.microLogger.MicroLogger;
 import kcl.teamIndexZero.traffic.log.outputs.Output;
 import kcl.teamIndexZero.traffic.log.outputs.Output_TERM;
 import kcl.teamIndexZero.traffic.log.outputs.Output_TXT;
 
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -16,34 +19,25 @@ public class Log_Config {
     private int global_log_level = Log_Levels.DEBUG; //Default
     private boolean log_exception_flag = true; //Default
     private Vector<Output> outputs = new Vector<Output>();
-
-    //TODO Create file loader class for getting options from outside
-    //TODO if file not found or empty then load defaults (txt)
+    private String config_file_name = "log_config.cfg";
 
     /**
      * Constructor (default)
      */
     public Log_Config() {
-            try {
-                //FileInput config_file = new FileInput("", "log_config.txt");
-
-                Log_TimeStamp time_stamp = new Log_TimeStamp();
-                global_file_name += time_stamp.getCustomStamp("yyyyMMdd'-'HHmmss");
-                //TODO load from file
-                //TODO if invalid/empty load defaults
-                //DEFAULTS
-                outputs.add(new Output_TERM("Console")); //Change to Output_TXT when implemented
-                outputs.add(new Output_TXT( global_file_name ) );
-            } catch ( IOException e ) {
-                //TODO config file cannot be accessed to default to terminal and txt(if that's doable)
-                MicroLogger.INSTANCE.log_Error( "IOException raised in [Log_Config.Log_Config()]" );
-                MicroLogger.INSTANCE.log_ExceptionMsg( e );
-                e.printStackTrace();
+        try {
+            Log_TimeStamp time_stamp = new Log_TimeStamp();
+            global_file_name += time_stamp.getCustomStamp("yyyyMMdd'-'HHmmss");
+            if (!this.configurationLoader(config_file_name)) {
+                //DEFAULT Configuration
+                outputs.add(new Output_TERM("Console"));
+                outputs.add(new Output_TXT(global_file_name));
             }
-
-
-
-
+        } catch (IOException e) {
+            MicroLogger.INSTANCE.log_Error("IOException raised in [Log_Config.Log_Config()]");
+            MicroLogger.INSTANCE.log_ExceptionMsg(e);
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -67,9 +61,12 @@ public class Log_Config {
 
     /**
      * Gets the "log exceptions" flag
+     *
      * @return Flag state
      */
-    public boolean getLogExceptionFlag() { return this.log_exception_flag; }
+    public boolean getLogExceptionFlag() {
+        return this.log_exception_flag;
+    }
 
     /**
      * Gets the output pipes of the log
@@ -88,5 +85,29 @@ public class Log_Config {
     private void setGlobalLogLevel(int level) {
         if (level < Log_Levels.OFF) level = Log_Levels.OFF;
         this.global_log_level = level;
+    }
+
+    /**
+     * Config file loader
+     *
+     * @param file_name Name of the configuration file for the logger
+     * @return Success
+     */
+    private boolean configurationLoader(String file_name) {
+        try {
+            FileInput in = new FileInput("", this.config_file_name);
+            List<String> lines = in.read();
+            //TODO Implement check for lines and extraction/assignments of options
+            //TODO If it fails then microlog the failures and return false
+            return false; //TODO change that to 'true' once everything is implemented here
+        } catch (IOException e) {
+            MicroLogger.INSTANCE.log_Error("IOException raised in [Log_Config.configurationLoader( ", file_name, " )] Cannot read/load the config file. Reverting to defaults.. ");
+            MicroLogger.INSTANCE.log_ExceptionMsg(e);
+            return false;
+        } catch (InvalidPathException e) {
+            MicroLogger.INSTANCE.log_Error("InvalidPathException raised in [Log_Config.configurationLoader( ", file_name, " )] Cannot read/load the config file. Reverting to defaults.. ");
+            MicroLogger.INSTANCE.log_ExceptionMsg(e);
+            return false;
+        }
     }
 }
