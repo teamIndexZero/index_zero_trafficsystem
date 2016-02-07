b1335dacc49020f96332c56ac1e7f59df784ef14
E A. Davison <axedude1@gmail.com>
2016-02-07 15:16:45 +0000

    Log_TimeStamp bug where test would sometimes fail during certain periods of the day squashed (hopefully).

---
+++ log/src/test/java/kcl/teamIndexZero/traffic/log/microLogger/MicroLoggerTest.java
@@ -0,0 +1,36 @@
+package kcl.teamIndexZero.traffic.log.microLogger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alwlyan on 07/02/2016.
+ */
+public class MicroLoggerTest {
+
+    @Test
+    public void testLog_Fatal() throws Exception {
+
+    }
+
+    @Test
+    public void testLog_Error() throws Exception {
+
+    }
+
+    @Test
+    public void testLog_Warning() throws Exception {
+
+    }
+
+    @Test
+    public void testLog() throws Exception {
+
+    }
+
+    @Test
+    public void testLog_Debug() throws Exception {
+
+    }
+}
---
+++ log/src/test/java/kcl/teamIndexZero/traffic/log/outputs/OutputTest.java
@@ -0,0 +1,36 @@
+package kcl.teamIndexZero.traffic.log.outputs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alwlyan on 07/02/2016.
+ */
+public class OutputTest {
+
+    @Test
+    public void testSetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOutputName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOutputType() throws Exception {
+
+    }
+
+    @Test
+    public void testOutput() throws Exception {
+
+    }
+
+    @Test
+    public void testOutput1() throws Exception {
+
+    }
+}
