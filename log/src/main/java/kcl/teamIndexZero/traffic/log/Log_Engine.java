db3547e314c15a21ee82cfdc68d64f06e5c9b41e
E. A. Davison <axedude1@gmail.com>
2016-02-15 12:20:32 +0000

    Small tweak in main classes to make them more testable + depreciation of unused method(s) in Log_Engine

--- log/src/main/java/kcl/teamIndexZero/traffic/log/Log_Engine.java
+++ log/src/main/java/kcl/teamIndexZero/traffic/log/Log_Engine.java
@@ -1,5 +1,6 @@
 package kcl.teamIndexZero.traffic.log;
 
+import kcl.teamIndexZero.traffic.log.microLogger.MicroLogger;
 import kcl.teamIndexZero.traffic.log.outputs.Output;
 
 /**
@@ -7,28 +8,39 @@ import kcl.teamIndexZero.traffic.log.outputs.Output;
  * Log engine
  */
 public class Log_Engine {
+    private static Log_Engine instance;
     private Log_Config global_config;
     private Long session_msg_number;
-    private static Log_Engine INSTANCE;
+
+    /**
+     * Gets the Log_Engine instance
+     *
+     * @return Log_Engine instance
+     * @throws NullPointerException when the instance is not initiated
+     */
+    public static Log_Engine getInstance() throws NullPointerException {
+        if (instance == null) {
+            MicroLogger.INSTANCE.log_Fatal("[Log_Engine.getInstance()] The static instance of the Log_Engine is null. Loader [.load( log_Engine locator ) was likely not called previous to [.getInstance()].");
+            throw new NullPointerException();
+        }
+        return instance;
+    }
+
+    /**
+     * Sets the Log_Engine instance
+     *
+     * @param locator new Log_Engine instance
+     */
+    public static void load(Log_Engine locator) {
+        instance = locator;
+    }
 
     /**
      * Constructor
      */
-    private Log_Engine() {
-        global_config = new Log_Config();
+    public Log_Engine(Log_Config config) {
+        global_config = config;
         session_msg_number = new Long(0);
-    }
-
-    /**
-     * Gets the instance of the Log_Engine
-     *
-     * @return Log_Engine instance
-     */
-    public static Log_Engine getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new Log_Engine();
-        }
-        return INSTANCE;
     }
 
     /**
@@ -55,10 +67,10 @@ public class Log_Engine {
      * @param class_origin Name of the message's origin
      * @param e            Exception raised
      */
-    protected void processException(Log_TimeStamp time_stamp, String class_origin, Exception e ) {
-        if (global_config.getGlobalLogLevel() > 0 && global_config.getLogExceptionFlag() ) {
+    protected void processException(Log_TimeStamp time_stamp, String class_origin, Exception e) {
+        if (global_config.getGlobalLogLevel() > 0 && global_config.getLogExceptionFlag()) {
             for (Output out : global_config.getOutputs()) {
-                out.output(class_origin, time_stamp, this.session_msg_number, e );
+                out.output(class_origin, time_stamp, this.session_msg_number, e);
             }
         }
         session_msg_number++;
