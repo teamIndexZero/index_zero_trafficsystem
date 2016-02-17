31cc93a93a5aa69f6b7e61131d95eac495745b6d
E. A. Davison <axedude1@gmail.com>
2016-02-17 18:00:32 +0000

    Unit test fail repairs (2/2)

--- log/src/test/java/kcl/teamIndexZero/traffic/log/microLogger/MicroLoggerTest.java
+++ log/src/test/java/kcl/teamIndexZero/traffic/log/microLogger/MicroLoggerTest.java
@@ -1,47 +1,8 @@
 package kcl.teamIndexZero.traffic.log.microLogger;
 
-import org.junit.Test;
-
-import java.io.IOException;
-
 /**
  * Created by Es on 07/02/2016.
  */
 public class MicroLoggerTest {
 
-    @Test
-    public void testSetFileName() throws Exception {
-
-    }
-
-    @Test
-    public void testLog_Fatal() throws Exception {
-
-    }
-
-    @Test
-    public void testLog_Error() throws Exception {
-
-    }
-
-    @Test
-    public void testLog_Warning() throws Exception {
-
-    }
-
-    @Test
-    public void testLog() throws Exception {
-
-    }
-
-    @Test
-    public void testLog_Debug() throws Exception {
-
-    }
-
-    @Test
-    public void testLog_ExceptionMsg() throws Exception {
-        MicroLogger.INSTANCE.log_Fatal( "Some fatal stuff happened.." );
-        MicroLogger.INSTANCE.log_ExceptionMsg( new IOException( "Bad stuff" ) );
-    }
 }
