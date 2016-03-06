f85c1e1e5faf29e65055ec8b1d19d0fc16d126d1
E. A. Davison <axedude1@gmail.com>
2016-03-06 13:00:56 +0000

    JunctionLink.getNextFeature() added  (+ test)

---
+++ simulator/src/test/java/kcl/teamIndexZero/traffic/simulator/data/links/JunctionLinkTest.java
@@ -0,0 +1,57 @@
+package kcl.teamIndexZero.traffic.simulator.data.links;
+
+import kcl.teamIndexZero.traffic.simulator.data.ID;
+import kcl.teamIndexZero.traffic.simulator.data.features.Junction;
+import kcl.teamIndexZero.traffic.simulator.data.features.Road;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Es on 06/03/2016.
+ */
+public class JunctionLinkTest {
+    private Road mockedRoad;
+    private Junction mockedJunction;
+    private JunctionLink jl;
+
+    @Before
+    public void setUp() throws Exception {
+        mockedRoad = mock(Road.class);
+        mockedJunction = mock(Junction.class);
+        jl = new JunctionLink(new ID("JunctionLinkTest"), mockedRoad, mockedJunction);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        jl = null;
+        mockedJunction = null;
+        mockedRoad = null;
+    }
+
+    @Test
+    public void testGetLinks() throws Exception {
+        when(mockedJunction.getLinks()).thenReturn(new ArrayList<Link>());
+        jl.getLinks();
+        verify(mockedJunction).getLinks();
+    }
+
+    @Test
+    public void testGetRoadID() throws Exception {
+        when(mockedRoad.getID()).thenReturn(new ID("Awesome Rd."));
+        assertEquals("Awesome Rd.", jl.getRoadID().toString());
+        verify(mockedRoad).getID();
+    }
+
+    @Test
+    public void testGetNextFeature() throws Exception {
+        assertEquals(mockedJunction, jl.getNextFeature());
+
+
+    }
+}
