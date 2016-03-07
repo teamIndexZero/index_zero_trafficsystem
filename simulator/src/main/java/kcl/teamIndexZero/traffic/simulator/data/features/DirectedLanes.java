1d91c0f0c63866fb49d08325017680bdc4b077e0
E. A. Davison <axedude1@gmail.com>
2016-03-07 12:34:27 +0000

    DirectedLanes docs updated

--- simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/features/DirectedLanes.java
+++ simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/features/DirectedLanes.java
@@ -42,6 +42,13 @@ public class DirectedLanes {
         return this.lanes.size();
     }
 
+    /**
+     * Gets a particular lane
+     *
+     * @param lane_index Index of the lane
+     * @return Lane
+     * @throws ArrayIndexOutOfBoundsException when trying to access a non existent lane in the list
+     */
     public ID getLaneID(int lane_index) throws ArrayIndexOutOfBoundsException {
         if (lane_index >= this.lanes.size()) {
             LOG.log_Error("Lane index '", lane_index, "' is not in the lanes available ('", this.lanes.size(), "').");
@@ -50,10 +57,20 @@ public class DirectedLanes {
         return lanes.get(lane_index).getID();
     }
 
+    /**
+     * Gets the road the directed lanes belong to
+     *
+     * @return Parent road
+     */
     public Road getRoad() {
         return road;
     }
 
+    /**
+     * Gets the lanes inside the group of directed lanes
+     *
+     * @return List of lanes
+     */
     public List<Lane> getLanes() {
         return lanes;
     }
