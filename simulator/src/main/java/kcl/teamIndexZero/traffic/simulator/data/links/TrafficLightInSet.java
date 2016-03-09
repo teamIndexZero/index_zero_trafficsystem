731f6ae818b912d201c84e02b1d6d5fd16dc167c
Oleksandr Cherednychenko <lexaux@gmail.com>
2016-03-09 10:14:05 +0000

    Fixed project to be actually buildable.

--- simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/links/TrafficLightInSet.java
+++ simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/links/TrafficLightInSet.java
@@ -3,31 +3,32 @@ package kcl.teamIndexZero.traffic.simulator.data.links;
 import kcl.teamIndexZero.traffic.simulator.ISimulationAware;
 import kcl.teamIndexZero.traffic.simulator.data.ID;
 import kcl.teamIndexZero.traffic.simulator.data.SimulationTick;
-import kcl.teamIndexZero.traffic.simulator.data.descriptors.TrafficLightRule;
-import kcl.teamIndexZero.traffic.simulator.data.links.TrafficLightSet;
 
 import java.util.Date;
 
 /**
  * Created by Es on 02/03/2016.
  */
-public abstract class TrafficLightInSet implements ISimulationAware {
-    private TrafficLight model;
-    private TrafficLightSet modelSet;
-    long CurrentDate = (new Date().getTime())/1000;
+public /*abstract*/ class TrafficLightInSet extends TrafficLight implements ISimulationAware {
+    long CurrentDate = (new Date().getTime()) / 1000;
     long lastChange = CurrentDate;
     long timer;
+    private TrafficLight model;
+    private TrafficLightSet modelSet;
 
-
+    public TrafficLightInSet(ID linkId) {
+        super(linkId);
+        //TODO Fix construction?
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void tick(SimulationTick tick) {
-        CurrentDate = (new Date().getTime())/1000;
+        CurrentDate = (new Date().getTime()) / 1000;
 
-        if (CurrentDate - lastChange> timer) {
+        if (CurrentDate - lastChange > timer) {
             modelSet.changeColour(model, model.currentState);
             lastChange = CurrentDate;
         }
--- simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/links/TrafficLightSet.java
+++ simulator/src/main/java/kcl/teamIndexZero/traffic/simulator/data/links/TrafficLightSet.java
@@ -1,11 +1,8 @@
 package kcl.teamIndexZero.traffic.simulator.data.links;
 
-import javafx.beans.binding.ListBinding;
 import kcl.teamIndexZero.traffic.simulator.ISimulationAware;
 import kcl.teamIndexZero.traffic.simulator.data.ID;
 import kcl.teamIndexZero.traffic.simulator.data.SimulationTick;
-import kcl.teamIndexZero.traffic.simulator.data.descriptors.TrafficLightRule;
-import kcl.teamIndexZero.traffic.simulator.data.links.TrafficLight;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,26 +12,24 @@ import java.util.Map;
  * Created by Es on 02/03/2016.
  */
 
-public class TrafficLightSet implements ISimulationAware{
+public class TrafficLightSet implements ISimulationAware {
 
     List<TrafficLight> lights = new ArrayList<>();
     private ID id;
     private TrafficLight trafficLight;
-    private Map <ID,TrafficLight>;
+    private Map<ID, TrafficLight> map;
 
 
     public Map createLightsmap(ID id, TrafficLight trafficLight) {
-     Map.put(id, trafficLight);
+        map.put(id, trafficLight);
+        return map;
     }
 
 
-    public TrafficLight.State changeColour(TrafficLight trafficLight, TrafficLight.State currentState){
-
-     TrafficLight.State temp;
-
-     if (currentState == TrafficLight.State.RED) ? temp = TrafficLight.State.GREEN : temp =  TrafficLight.State.RED ;
-
-      return temp;
+    public TrafficLight.State changeColour(TrafficLight trafficLight, TrafficLight.State currentState) {
+        TrafficLight.State temp;
+        temp = (currentState == TrafficLight.State.RED) ? TrafficLight.State.GREEN : TrafficLight.State.RED;
+        return temp;
     }
 
     public void addLights(TrafficLight light) {
