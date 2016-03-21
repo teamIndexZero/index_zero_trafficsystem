1e952516e36e48d1d133a7a1abbdefc312628be9
Inspiron17R <werf@onet.pl>
2016-03-21 00:21:03 +0000

    Further tests.

--- simulator/src/test/java/kcl/teamIndexZero/traffic/simulator/data/trafficLight/TrafficLightControllerTest.java
+++ simulator/src/test/java/kcl/teamIndexZero/traffic/simulator/data/trafficLight/TrafficLightControllerTest.java
@@ -1,6 +1,10 @@
 package kcl.teamIndexZero.traffic.simulator.data.trafficLight;
 
+import kcl.teamIndexZero.traffic.simulator.data.ID;
+import kcl.teamIndexZero.traffic.simulator.data.descriptors.TrafficLightRule;
+import kcl.teamIndexZero.traffic.simulator.data.descriptors.TrafficLightsInSetRule;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,23 +22,53 @@ import static org.junit.Assert.*;
  * Created by JK on 14-03-16.
  */
 public class TrafficLightControllerTest {
-    private TrafficLightController controllerModel;
+
+    private TrafficLightController model;
+    private TrafficLightRule rule;
+    private TrafficLightsInSetRule setRule;
+    private TrafficLight trafficLight;
+    private TrafficLightInSet trafficLightInSet;
+    private TrafficLightSet trafficLightSet;
+    private boolean flag = false;
     private Date date;
 
 
     @Before
     public void setUp() throws Exception {
-        controllerModel = new TrafficLightController();
+        model = new TrafficLightController();
+        rule = new TrafficLightRule();
+        setRule = new TrafficLightsInSetRule();
+        trafficLightSet = new TrafficLightSet(new ID("TrafficLightSetTest"));
+        trafficLight = new TrafficLight(new ID("TrafficLightTest"));
+        trafficLightInSet= new TrafficLightInSet(new ID("TrafficLightInSetTest"));
     }
 
     @After
     public void tearDown() throws Exception {
-        controllerModel = null;
+        model = null;
+        rule = null;
+        setRule = null;
+        trafficLightSet = null;
+        trafficLight = null;
+        trafficLightInSet = null;
     }
 
     @Test
-    public void test() {
+    public void testGetSingleSet() {
+        Assert.assertEquals(model.getSingleSet(), model.TrafficLightSinglesList);
+    }
 
+    @Test
+    public void testAddRuleSingleTF() {
+        model.addTrafficlight(trafficLight);//At the beginning the State was GREEN
+        model.addRule(rule); //should be RED now
 
+        for (TrafficLight tf : model.TrafficLightSinglesList) {
+            if (tf.currentState == TrafficLightState.RED) {
+                flag = true;
+            }
+            ;
+        }
+        Assert.assertTrue(flag == true);
     }
 }
