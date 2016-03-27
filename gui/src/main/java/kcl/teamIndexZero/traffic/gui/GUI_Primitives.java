5d6ef57cd929e79cc6ad46bfc1e02709d644ee91
kumar awijeet <kumarawijeet@gmail.com>
2016-03-27 19:40:04 +0100

    Cleaning up of unused code

--- gui/src/main/java/kcl/teamIndexZero/traffic/gui/GUI_Primitives.java
+++ gui/src/main/java/kcl/teamIndexZero/traffic/gui/GUI_Primitives.java
@@ -5,7 +5,6 @@ import kcl.teamIndexZero.traffic.log.Logger_Interface;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.QuadCurve2D;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,22 +17,6 @@ public class GUI_Primitives {
     InputStream imageStream;
     int j = 0, k = 0;
     Image image, image1;
-    public void drawLine(int x1, int y1, int x2, int y2, Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        g.drawLine(x1, y1, x2, y2);
-    }
-
-    public void drawCurve(int x1, int y1, int x2, int y2, int ctrlx, int ctrly, Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        QuadCurve2D q = new QuadCurve2D.Float();
-        q.setCurve(x1, y1, ctrlx, ctrly, x2, y2);
-        g2.draw(q);
-    }
-
-    public void drawSingleRoad(int x, int y, int x1, int y1, int x2, int y2, int x3, int y3, Graphics g) {
-        drawLine(x, y, x1, y1, g);
-        drawLine(x2, y2, x3, y3, g);
-    }
 
     public void drawSmallCar(int x3, int y3, double angleToXAxis, Graphics g) throws IOException {
         drawCar(x3, y3, angleToXAxis, "gui/src/main/resources/sprites/bmw_z_top_view_clip_art_18132.jpg", g);
